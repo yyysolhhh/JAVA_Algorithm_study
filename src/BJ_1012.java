91f8cf3ae95882b78f51abf6f0abb5edfe1151bf
yyysolhhh <solbaram37@gmail.com>
2022-01-14 22:22:38 +0900

    220114

--- src/BJ_1012.java
+++ src/BJ_1012.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class BJ_1012 {
@@ -14,6 +16,7 @@ public class BJ_1012 {
 			K = sc.nextInt();
 			map = new int[N+1][M+1];
 			visited = new boolean[N+1][M+1];
+			int res = 0;
 			
 			for (int j = 0; j < K; j++) {
 				int X = sc.nextInt();
@@ -21,10 +24,11 @@ public class BJ_1012 {
 				map[Y][X] = 1;
 			}
 			
-			for (int j = 0; j < N; j++) {
-				for (int j2 = 0; j2 < M; j2++) {
+			for (int j = 0; j < M; j++) {
+				for (int j2 = 0; j2 < N; j2++) {
 					if (map[j][j2] == 1) {
 						BFS(j, j2);
+						res++;
 					}
 				}
 			}
@@ -32,6 +36,7 @@ public class BJ_1012 {
 	}
 	
 	public static void BFS(int y, int x) {
+		Queue<Integer> queue = new LinkedList<>();
 		
 	}
 
