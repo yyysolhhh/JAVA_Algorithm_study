02b495ec10eab95fd933fb9ca29c9c3c10252949
yyysolhhh <solbaram37@gmail.com>
2022-01-18 02:09:40 +0900

    220118 에러

--- src/BJ_1012.java
+++ src/BJ_1012.java
@@ -1,21 +1,33 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+class position {
+	int x;
+	int y;
+	position(int y, int x) {
+		this.x = x;
+		this.y = y;
+	}
+}
 
 public class BJ_1012 {
 	static int M, N, K;
+	
 	static int[][] map;
 	static boolean[][] visited;
+	
+	static int[] dy = {-1, 1, 0, 0};
+	static int[] dx = {0, 0, -1, 1};
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		for (int i = 0; i < T; i++) {
+		for (int i = 0; i < T+1; i++) {
 			M = sc.nextInt();
 			N = sc.nextInt();
 			K = sc.nextInt();
-			map = new int[N+1][M+1];
-			visited = new boolean[N+1][M+1];
+			map = new int[N][M];
+			visited = new boolean[N][M];
 			int res = 0;
 			
 			for (int j = 0; j < K; j++) {
@@ -24,20 +36,43 @@ public class BJ_1012 {
 				map[Y][X] = 1;
 			}
 			
-			for (int j = 0; j < M; j++) {
-				for (int j2 = 0; j2 < N; j2++) {
-					if (map[j][j2] == 1) {
-						BFS(j, j2);
+			for (int j = 0; j < N; j++) {
+				for (int k = 0; k < M; k++) {
+					if (map[j][k] == 1 && !visited[i][j]) {
+						BFS(j, k);
 						res++;
 					}
 				}
 			}
+			
+			System.out.println(res);
 		}
 	}
 	
 	public static void BFS(int y, int x) {
-		Queue<Integer> queue = new LinkedList<>();
+		Queue<position> queue = new LinkedList<>();
+		position p = new position(y, x);
+		queue.offer(p);
 		
+		while (!queue.isEmpty()) {
+			position p1 = queue.poll();
+			visited[p1.y][p1.x] = true;
+			
+			for (int i = 0; i < 4; i++) {
+				int ny = p1.y + dy[i];
+				int nx = p1.x + dx[i];
+				
+				if (ny >= 0 && nx >= 0 && ny < N && nx < M) {
+					if (map[ny][nx] == 1 && !visited[ny][nx]) {
+						position p2 = new position(ny, nx);
+						queue.offer(p2);
+						visited[p2.y][p2.x] = true;
+					}
+				}
+				
+				
+			}
+		}
 	}
 
 }
