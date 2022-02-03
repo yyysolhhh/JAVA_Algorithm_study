48a941afeb1cc839f6591029b5349481c5524726
yyysolhhh <solbaram37@gmail.com>
2022-02-04 00:33:51 +0900

    220204 별찍기

---
+++ src/BJ_2442.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+// 맞음
+public class BJ_2442 {
+    static int N;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
