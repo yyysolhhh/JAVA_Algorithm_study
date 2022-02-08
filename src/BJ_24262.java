5f1ea1ddb3df7f5c03dd8c241aab32f283d8021f
yyysolhhh <solbaram37@gmail.com>
2022-02-09 00:03:02 +0900

    220209

---
+++ src/BJ_24262.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BJ_24262 {
+    static int n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
