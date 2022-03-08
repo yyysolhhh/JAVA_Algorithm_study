d46f87a6f884d4bddf74b33731dace8c9881feb0
yyysolhhh <solbaram37@gmail.com>
2022-03-08 23:15:24 +0900

    220306 unsolved Z

---
+++ src/BJ_1074.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ_1074 {
+    static int N, r, c;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        
+    }
+}
