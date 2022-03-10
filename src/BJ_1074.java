d526bdd05939223f831d36b1abe879e27264a481
yyysolhhh <solbaram37@gmail.com>
2022-03-11 00:33:08 +0900

    220311 Z

--- src/BJ_1074.java
+++ src/BJ_1074.java
@@ -4,49 +4,39 @@ import java.util.StringTokenizer;
 public class BJ_1074 {
     static int N, r, c;
     static int order;
-    static int size;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
-        size = (int) Math.pow(2, N);
+        int size = (int) Math.pow(2, N);
         order = 0;
 
         search(r, c, size);
-        bw.write(order);
-        bw.flush();
-        bw.close();
+        System.out.println(order);
     }
 
     static void search(int r, int c, int size) {
-        System.out.println(size);
-        System.out.println(order);
         if (size == 1)
-            return ;
-        // 1사분면
+            return;
         if (r < size / 2 && c < size / 2) {
             search(r, c, size / 2);
         }
-        // 2사분면
         else if (r < size / 2 && c >= size / 2) {
             order += size * size / 4;
-            search(r, c + size / 2, size / 2);
+            search(r, c - size / 2, size / 2);
         }
-        // 3사분면
         else if (r >= size / 2 && c < size / 2) {
-            order += size * size / 4 + 1;
-            search(r + size / 2, c, size / 2);
+            order += 2 * (size * size / 4);
+            search(r - size / 2, c, size / 2);
         }
-        // 4사분면
         else {
-            order += size * size / 4 + 2;
-            search(r + size / 2, c + size / 2, size / 2);
+            order += 3 * (size * size / 4);
+            search(r - size / 2, c - size / 2, size / 2);
         }
     }
 }
