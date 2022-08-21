192f92ee99f24c0224e348c9d9de702c4ac1722a
yyysolhhh <solbaram37@gmail.com>
2022-08-21 19:55:22 +0900

    220821 / bj13458 시험 감독 / math

--- src/baekjoon/BJ_13460.java
+++ src/baekjoon/BJ_13460.java
@@ -35,7 +35,6 @@ public class BJ_13460 {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
